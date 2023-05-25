d5765bb10417035efb16f4b484c0f998030fc977
Serkan Babacan <Fhourch4n@gmail.com>
2023-05-25 22:56:33 +0300

    snake completed

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from turtle import *
+
+
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My sanke game")
+screen.tracer(0)
---
+++ sanke.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+        self.head = self.snakes[0]
+    def create_snake(self):
